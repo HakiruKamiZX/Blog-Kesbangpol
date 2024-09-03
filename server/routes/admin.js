a854178a5aed70df5bc779a4cc576dd9c632e88a
HakiruKamiZX <crimsonzx890@gmail.com>
2024-09-03 09:41:46 +0700

    Update : Polishing registartion method

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const Post = require('../models/post');
+const Post = require('../models/Post');
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 
 const adminLayout= '../views/layouts/admin.ejs'
 
@@ -25,11 +28,11 @@ router.get ('/admin', async (req, res) => {
 });
 
 /**
- * GET /
+ * POST /
  * ADMIN CHECKLOGIN
  */
 
-router.post ('/admin', async (req, res) => {
+router.post('/admin', async (req, res) => {
     try {
 
         const { username, password } = req.body;
@@ -40,5 +43,31 @@ router.post ('/admin', async (req, res) => {
     }
      
 });
+
+/**
+ * POST /
+ * ADMIN REGISTER
+ */
+
+router.post('/register', async (req, res) => {
+    try {
+        console.log('Request body:', req.body);  // Log the request body
+        const { username, password } = req.body;
+
+        // Additional validation if needed
+        if (!username || !password) {
+            return res.status(400).json({ message: 'Username and password are required' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({ username, password: hashedPassword });
+        res.status(201).json({ message: 'User Created', user });
+    } catch (error) {
+        console.error('Error creating user:', error);
+        res.status(500).json({ message: 'Internal server error during registration' });
+    }
+});
+
+
 
 module.exports = router;
